ff92e3b771c84756b2778915339c5a087364a9eb
Vignesh <vignesh@valtanix.net>
2018-03-23 00:45:37 +0530

    react-redux example

---
+++ BooksList/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts', pages: 150},
+		{ title: 'Harry Potter', pages: 1103},
+		{ title: 'The Dark Tower', pages: 245},
+		{ title: 'Eloquent Ruby', pages: 10}						
+	]
+}
